99b8377aa52e517b8984617f7a1fa6acdd7005cb
Virendra Ashiwal <ashiwalv@gmail.com>
2020-12-29 20:14:16 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
-	fmt.Fprintf(w, "Hello India, it is %d:%d", t.Hour(), getMinute(t.Minute(), t.Second()))
+	fmt.Fprintf(w, "Hello Linz, it is %d:%d", t.Hour(), getMinute(t.Minute(), t.Second()))
 
 	Fibonacci(1000)
 }
